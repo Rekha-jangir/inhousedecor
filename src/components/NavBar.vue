2bb88863a9e93e245b91271f90f8c830f4a84f58
Rekha <rekhajanggir6789@gmail.com>
2023-04-25 11:02:10 +0530

    added home and about page component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+
+<template>
+    <div class="container">
+        <div class="nav-left">
+            <div class="logo">
+                <img src="../assets/logo.jpeg" alt="">
+            </div>
+        </div>
+        <div class="nav-right">
+            <div class="nav-item">
+                <RouterLink class="nav-item" to="home">Home</RouterLink>
+                <RouterLink class="nav-item" to="about">About</RouterLink>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.logo img{
+    height: 10vh;
+    width: 60px;
+}
+.container{
+}
+</style>
